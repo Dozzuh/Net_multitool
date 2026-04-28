06afc6c1c41026b5440a6b61bd14b25bd0931aba
Dozzuh <105948152+Dozzuh@users.noreply.github.com>
2026-04-27 22:24:09 -0700

    Add files via upload
    Added a little bit of error handling and ability to scan multiple ports one at a time

--- port_functions.py
+++ port_functions.py
@@ -1,19 +1,25 @@
 import socket
+# add error handling for only port range numbers, and to make sure the end number is not smaller than the first number.
 
 
 def user_input():
     target_host = input("Enter a web address: ")
-    # i need error handling for non integer inputs
-    target_port = int(input("Enter a port number: "))
-    return target_host, target_port
+    while True:
+        try:
+            start_port = int(
+                input("Enter the start of the port range to scan: "))
+            end_port = int(input("Enter the end range of the port to scan: "))
+            return target_host, start_port, end_port
+        except:
+            print("Not a valid port number.\nValid port numbers are 0-65535")
 
 
 def connect(th, tp):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         client.connect((th, tp))
-        client.close()
         return "Open"
     except:
-        client.close()
         return "Closed"
+    finally:
+        client.close()
--- port_scan_main.py
+++ port_scan_main.py
@@ -1,6 +1,10 @@
 import socket  # This allows python to create network connections
 import port_functions
 
-tgt_host, tgt_port = port_functions.user_input()
-result = port_functions.connect(tgt_host, tgt_port)
-print(f"The port {tgt_port} on {tgt_host} is {result}")
+tgt_host, tgt_port, end_port = port_functions.user_input()
+# result = port_functions.connect(tgt_host, tgt_port)
+# print(f"The port {tgt_port} on {tgt_host} is {result}")
+
+for x in range(tgt_port, end_port + 1):
+    result = port_functions.connect(tgt_host, x)
+    print(f"The port {x} on host {tgt_host} is {result}")
