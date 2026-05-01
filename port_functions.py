d27e3e6e51bb15dd398c75b500dcb44e1627810a
Dozzuh <105948152+Dozzuh@users.noreply.github.com>
2026-04-30 19:08:32 -0700

    Threading changes (still in progress)
    adding to repo so I can edit on laptop and desktop

--- port_functions.py
+++ port_functions.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import time
 # add error handling for only port range numbers, and to make sure the end number is not smaller than the first number.
 
 
@@ -13,6 +15,8 @@ def user_input():
         except:
             print("Not a valid port number.\nValid port numbers are 0-65535")
 
+# This function is the actual check if a port is open or closed
+
 
 def connect(th, tp):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,3 +27,10 @@ def connect(th, tp):
         return "Closed"
     finally:
         client.close()
+
+
+# This function is going to replace the for loops purpose of iterating through each of the port numbers and scanning
+# thIs funcion basically takes uses the connect funtion and prints out the result
+def scan_port(host, port):
+    result = connect(host, port)
+    print(f"Port {port} is {result}")
--- port_scan_main.py
+++ port_scan_main.py
@@ -1,10 +1,22 @@
 import socket  # This allows python to create network connections
+import threading
+import time
 import port_functions
 
-tgt_host, tgt_port, end_port = port_functions.user_input()
-# result = port_functions.connect(tgt_host, tgt_port)
-# print(f"The port {tgt_port} on {tgt_host} is {result}")
 
+tgt_host, tgt_port, end_port = port_functions.user_input()
+
+threads = []
+
+# For loop job will switch to creating threads rather than iterating through each port to scan
+# changes from scanning ports to creating instances of scanning the port
 for x in range(tgt_port, end_port + 1):
-    result = port_functions.connect(tgt_host, x)
-    print(f"The port {x} on host {tgt_host} is {result}")
+    thread = threading.Thread(
+        target=port_functions.scan_port, args=(tgt_host, x))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+    # result = port_functions.connect(tgt_host, x)
+    # print(f"The port {x} on host {tgt_host} is {result}")
